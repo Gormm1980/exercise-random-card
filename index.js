851dbb8341d28e0ac1c409db3348034437b564bf
Gormm1980 <guillermosantamariarodrigo@hotmail.com>
2021-04-29 19:30:11 +0000

    first change

---
+++ index.js
@@ -0,0 +1,8 @@
+let num = Math.floor(Math.random()*(13-1) +1);
+let symbol = Math.floor(Math.random()*(5-1) +1);
+
+let number = document.querySelector("#numcenter");
+window.onload= function(){
+    number.innerHTML=num;}
+
+
