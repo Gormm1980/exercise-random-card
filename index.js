07ec5dc580ae766996a349380dbe33e655a07c26
Gormm1980 <guillermosantamariarodrigo@hotmail.com>
2021-05-13 21:07:10 +0000

    add content

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-let num = Math.floor(Math.random()*(13-1) +1);
-let symbol = Math.floor(Math.random()*(5-1) +1);
+window.onload = function(){
+  
+  var cardNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  var suit = ["Diamonds", "Spades", "Hearts", "Clubs"];
+  var randomCardNumber = Math.floor(Math.random() * cardNumber.length);
+  var randomSuitNumber = Math.floor(Math.random() * suit.length);
+  var finalSuit = suit[randomSuitNumber];
+  
+  document.getElementById('cardContent').innerHTML = cardNumber[randomCardNumber];
+  document.getElementById('theCard').className = "";
+  document.getElementById('theCard').classList.add("card");
+  document.getElementById('theCard').classList.add(getSuiteClass(finalSuit));
+  
+}
 
-let number = document.querySelector("#numcenter");
-window.onload= function(){
-    number.innerHTML=num;}
+function getSuiteClass(suit)
+{
+  switch (suit) {
+    case "Diamonds": return "suit-diamonds";
+
+    case "Spades": return "suit-spades";
+    
+    case "Hearts": return "suit-hearts"; 
+   
+    case "Clubs": return "suit-clubs";
+   
+  }
+}
 
 
